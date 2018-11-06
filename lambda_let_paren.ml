ac9d24638bfd9aefd4d578e40ea4f238178cf3fa
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-11-06 14:30:52 +0900

    Definition of the terms and values of \lambda^{let,\cdot}

---
+++ lambda_let_paren.ml
@@ -0,0 +1,27 @@
+type id = Syntax.id
+
+type idxvar = int
+
+type idx =
+  | IVar of idxvar
+  | INat of int
+
+type exp =
+  | EVar of id
+  | EInt of int
+  | EAbs of id * exp
+  | EApp of exp * exp
+  | ELet of id * exp * exp
+  | EArray of exp list
+  | EArrayGet of exp * idx
+  | EArrayModify of exp * idx * exp
+  | EIdxAbs of idx * exp
+  | EIdxApp of exp * idx
+
+type value =
+  | VInt of int
+  | VProc of id * exp * env
+  | VArray of value array
+  | VIdxAbs of idx * exp
+
+and env = value Environment.t

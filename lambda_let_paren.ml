05d9f4cd2403f4a9f80a95588c5ed74d42ca4412
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-11-06 20:48:53 +0900

    Define types of \lambda^{let,[]}

--- lambda_let_paren.ml
+++ lambda_let_paren.ml
@@ -6,6 +6,33 @@ type idx =
   | IVar of idxvar
   | INat of int
 
+type ty =
+  | TVar of tyvar
+  | TInt
+  | TFun of ty * ty
+  | TRecord of (label * ty) list
+  | TIdxFun of (label * ty) list * ty
+and kind =
+  | KUniv
+  | KRecord of (label * ty) list
+
+type polyty = Forall of (tyvar * kind) list * ty
+
+exception Label_not_found of label
+exception Undefined_index_value
+exception Not_a_record_type
+
+let idx_value l =
+  let rec inner n = function
+    | [] -> raise (Label_not_found l)
+    | (l', _) :: t ->
+        if l = l' then n
+        else inner (n + 1) t
+  in function
+  | TVar _ -> raise Undefined_index_value
+  | TRecord xs -> inner 1 xs
+  | _ -> raise Not_a_record_type
+
 type exp =
   | EVar of id
   | EInt of int

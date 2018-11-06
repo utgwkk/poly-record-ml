8e1ccf783036a25530bc74c2a5a6f128cbf81759
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-11-06 21:13:16 +0900

    Pass all tests at this time

--- compiler.ml
+++ compiler.ml
@@ -16,7 +16,7 @@ let rec monotycon = function
   | Lld.TInt -> Llp.TInt
   | Lld.TFun (t1, t2) -> Llp.TFun (monotycon t1, monotycon t2)
   | Lld.TRecord ts ->
-      let ts' = List.map (fun (l, t) -> (l, monotycon t)) ts in
+      let ts' = List.map (fun (l, t) -> (l, monotycon t)) ts |> List.sort compare in
       Llp.TRecord ts'
 
 let kcon = function
@@ -31,7 +31,50 @@ let rec tycon (Lld.Forall (xs, t)) =
   | [] -> Llp.Forall (xs', monotycon t)
   | _ -> Llp.Forall (xs', Llp.TIdxFun (idxsets, monotycon t))
 
-let rec compile (lbenv : Lld.lbenv) (tyenv : Lld.tyenv) (exp : Lld.exp) = Llp.EInt 1
+let rec compile lbenv tyenv = function
+  | Lld.EInt i -> Llp.EInt i
+  | Lld.EAbs (x, t, e) ->
+      let t' = Llp.Forall ([], monotycon t) in
+      let tyenv' = Environment.extend x t' tyenv in
+      let e' = compile lbenv tyenv' e in
+      Llp.EAbs (x, e')
+  | Lld.EApp (e1, e2) ->
+      let e1' = compile lbenv tyenv e1 in
+      let e2' = compile lbenv tyenv e2 in
+      Llp.EApp (e1', e2')
+  | Lld.ELet (x, pt, e1, e2) ->
+      let pt' = tycon pt in
+      let e1' = compile lbenv tyenv e1 in
+      let tyenv' = Environment.extend x pt' tyenv in
+      let e2' = compile lbenv tyenv' e2 in
+      Llp.ELet (x, e1', e2')
+  | Lld.ERecord xs ->
+      let xs' =
+        xs
+        |> List.sort compare
+        |> List.map (fun (_, e) -> compile lbenv tyenv e)
+      in
+      Llp.EArray xs'
+  | Lld.ERecordGet (e1, t, l) ->
+      let e1' = compile lbenv tyenv e1 in
+      let t' = monotycon t in
+      let idx = begin
+        try Llp.INat (Llp.idx_value l t')
+        with Llp.Undefined_index_value ->
+          Environment.lookup (l, t') lbenv
+      end
+      in Llp.EArrayGet (e1', idx)
+  | Lld.ERecordModify (e1, t, l, e2) ->
+      let e1' = compile lbenv tyenv e1 in
+      let t' = monotycon t in
+      let idx = begin
+        try Llp.INat (Llp.idx_value l t')
+        with Llp.Undefined_index_value ->
+          Environment.lookup (l, t') lbenv
+      end
+      in
+      let e2' = compile lbenv tyenv e2 in
+      Llp.EArrayModify (e1', idx, e2')
 
 (* entrypoint *)
 let start exp = compile Environment.empty Environment.empty exp

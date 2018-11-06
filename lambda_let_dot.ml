c5ccb88e37f0863da9326c03bdd17581ef029fc7
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-11-06 16:15:15 +0900

    Define types and terms of \Lambda^{let,\cdot}

---
+++ lambda_let_dot.ml
@@ -0,0 +1,34 @@
+type id = Syntax.id
+
+type idx = Lambda_let_paren.idx
+
+type tyvar = int
+
+type label = string
+
+type ty =
+  | TVar of tyvar
+  | TInt
+  | TFun of ty * ty
+  | TRecord of (label * ty) list
+
+and kind =
+  | KUniv
+  | KRecord of (label * ty) list
+
+type polyty = Forall of (tyvar * kind) list * ty
+
+type exp =
+  | EPolyInst of id * ty list
+  | EInt of int
+  | EAbs of id * ty * exp
+  | EApp of exp * exp
+  | EPolyGen of exp * polyty
+  | ELet of id * polyty * exp * exp
+  | ERecord of (label * exp) list
+  | ERecordGet of exp * ty * label
+  | ERecordModify of exp * ty * label * exp
+
+type tyenv = (id, polyty) Environment.t
+
+type lbenv = (label, idx) Environment.t

2479a6912cac17183126fb1c666b044510419633
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-11-08 18:04:28 +0900

    Duplication test

--- typechecker_test.ml
+++ typechecker_test.ml
@@ -5,13 +5,6 @@ open Lambda_let_dot
 
 let tests = "Typechecker_test">:::[
   "partial_check">:::[
-    "int">::(fun ctxt ->
-      let kenv = Environment.empty in
-      let tyenv = Environment.empty in
-      let input = EInt 1 in
-      let expected = Forall ([], TInt) in
-      assert_equal expected (type_check kenv tyenv input)
-    );
     "x:forall X::U Y::U, X->Y->X |- x[int,int] : int->int->int">::(fun ctxt ->
       let kenv = Environment.empty in
       let tyenv =

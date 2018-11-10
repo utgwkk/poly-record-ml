5f69529e699adbb3080ab418997f25480e8ba29a
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-11-10 18:32:42 +0900

    handle type / kind check failure and retry

--- main.ml
+++ main.ml
@@ -5,14 +5,26 @@ let if_debug f = if !debug then f ()
 let rec repl () =
   print_string "# ";
   flush stdout;
+
   let exp = Parser.main Lexer.main (Lexing.from_channel stdin) in
   if_debug (fun () -> Printf.printf "(* Input *)\n%s\n\n" (ExplicitlyTyped.string_of_exp exp));
-  let pty = Typechecker.start exp in
-  if_debug (fun () -> Printf.printf "(* Type check *)\n%s\n\n" (ExplicitlyTyped.string_of_polyty pty));
+
+  begin try
+    let pty = Typechecker.start exp in
+    if_debug (fun () -> Printf.printf "(* Type check *)\n%s\n\n" (ExplicitlyTyped.string_of_polyty pty));
+  with
+    | Typechecker.Typecheck_failed -> retry "[ERROR] type check failed"
+    | Typechecker.Kindcheck_failed -> retry "[ERROR] kind check failed"
+  end;
+
   let compiled = Compiler.start exp in
   if_debug (fun () -> Printf.printf "(* Compiled exp *)\n%s\n\n" (Implementation.string_of_exp compiled));
   let value = Evaluator.start compiled in
   Printf.printf "val - = %s\n" (Implementation.string_of_value value);
+  repl ()
+
+and retry s =
+  print_endline s;
   repl ()
 
 let main () =

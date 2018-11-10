67bd3b35130eec7d8a1e8bf4419ff37a1fa6efbc
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-11-10 18:19:06 +0900

    Debug output mode

--- main.ml
+++ main.ml
@@ -1,14 +1,25 @@
+let debug = ref false
+
+let if_debug f = if !debug then f ()
+
 let rec repl () =
   print_string "# ";
   flush stdout;
   let exp = Parser.main Lexer.main (Lexing.from_channel stdin) in
-  ignore @@ Typechecker.start exp;
+  let pty = Typechecker.start exp in
+  if_debug (fun () -> Printf.printf "(* Type check *)\n%s\n\n" (ExplicitlyTyped.string_of_polyty pty));
   let compiled = Compiler.start exp in
+  if_debug (fun () -> Printf.printf "(* Compiled exp *)\n%s\n\n" (Implementation.string_of_exp compiled));
   let value = Evaluator.start compiled in
   Printf.printf "val - = %s\n" (Implementation.string_of_value value);
   repl ()
 
 let main () =
+  let usage = Printf.sprintf "%s [--debug]" Sys.argv.(0) in
+  let arg = Arg.align [
+    ("--debug", Arg.Set debug, "print debug output.");
+  ] in
+  Arg.parse arg (fun _ -> ()) usage;
   repl ()
 
 let () = main ()

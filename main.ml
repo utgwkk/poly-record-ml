9625c9c00e9f200f58427ee664d7e4bb323f53ac
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-11-11 12:58:14 +0900

    can specify input file name

--- main.ml
+++ main.ml
@@ -1,38 +1,47 @@
 let debug = ref false
+let srcfile = ref "-"
 
 let if_debug f = if !debug then f ()
 
-let rec repl () =
-  print_string "# ";
+let rec compile prompt chan k =
+  print_string prompt;
   flush stdout;
 
-  let exp = Parser.main Lexer.main (Lexing.from_channel stdin) in
+  let exp = Parser.main Lexer.main (Lexing.from_channel chan) in
   if_debug (fun () -> Printf.printf "(* Input *)\n%s\n\n" (ExplicitlyTyped.string_of_exp exp));
 
   begin try
     let pty = Typechecker.start exp in
     if_debug (fun () -> Printf.printf "(* Type check *)\n%s\n\n" (ExplicitlyTyped.string_of_polyty pty));
   with
-    | Typechecker.Typecheck_failed -> retry "[ERROR] type check failed"
-    | Typechecker.Kindcheck_failed -> retry "[ERROR] kind check failed"
+    | Typechecker.Typecheck_failed -> retry k "[ERROR] type check failed"
+    | Typechecker.Kindcheck_failed -> retry k "[ERROR] kind check failed"
   end;
 
   let compiled = Compiler.start exp in
   if_debug (fun () -> Printf.printf "(* Compiled exp *)\n%s\n\n" (Implementation.string_of_exp compiled));
   let value = Evaluator.start compiled in
   Printf.printf "val - = %s\n" (Implementation.string_of_value value);
-  repl ()
+  k ()
 
-and retry s =
+and retry k s =
   print_endline s;
-  repl ()
+  k ()
 
 let main () =
-  let usage = Printf.sprintf "%s [--debug]" Sys.argv.(0) in
+  let usage = Printf.sprintf "%s [--debug] [filename]" Sys.argv.(0) in
   let arg = Arg.align [
     ("--debug", Arg.Set debug, "print debug output.");
   ] in
-  Arg.parse arg (fun _ -> ()) usage;
-  repl ()
+  Arg.parse arg (fun s -> srcfile := s) usage;
+
+  if !srcfile = "-" then
+    let chan = stdin in
+    let rec k () = compile "# " chan k in
+    compile "# " chan k
+  else
+    let chan = open_in !srcfile in
+    let rec k () = close_in chan in
+    compile "" chan k
 
 let () = main ()

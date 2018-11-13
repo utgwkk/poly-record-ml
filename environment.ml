2391e57a48cfcdb68eb40764d625e2aea1f4231b
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-11-13 13:30:33 +0900

    add Environment.lookup_opt

--- environment.ml
+++ environment.ml
@@ -8,6 +8,8 @@ let extend x v env = (x, v) :: env
 
 let rec lookup x env = try List.assoc x env with Not_found -> raise Not_bound
 
+let rec lookup_opt x env = List.assoc_opt x env
+
 let rec map f = function
   | [] -> []
   | (id, v) :: rest -> (id, f v) :: map f rest

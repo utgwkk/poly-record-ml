dd455d6b4e891abec7026a345cba8e068ba6530a
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-11-13 13:30:33 +0900

    add Environment.lookup_opt

--- environment.mli
+++ environment.mli
@@ -5,6 +5,7 @@ exception Not_bound
 val empty : ('a, 'b) t
 val extend : 'a -> 'b -> ('a, 'b) t -> ('a, 'b) t
 val lookup : 'a -> ('a, 'b) t -> 'b
+val lookup_opt : 'a -> ('a, 'b) t -> 'b option
 val map : ('b -> 'c) -> ('a, 'b) t -> ('a, 'c) t
 val fold_right : ('b -> 'c -> 'c) -> ('a, 'b) t -> 'c -> 'c
 
